ed169f3399b4bb5a55c342741fd634f1eb8f4aa9
Sarthak <Sarthak>
2017-01-27 11:40:14 +0530

    Android 6.0+ permission bug fix

--- app/src/main/java/com/loku/sarthak/lokuapp/ReaderActivity.java
+++ app/src/main/java/com/loku/sarthak/lokuapp/ReaderActivity.java
@@ -1,10 +1,18 @@
 package com.loku.sarthak.lokuapp;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.graphics.PointF;
+import android.hardware.Camera;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import com.google.zxing.ResultPoint;
@@ -18,6 +26,8 @@ import java.util.List;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
+import static android.os.Build.VERSION.SDK;
+
 /**
  * Created by SARTHAK on 1/26/2017.
  */
@@ -25,6 +35,8 @@ public class ReaderActivity extends AppCompatActivity implements ReaderScreenCon
 
     @Bind(R.id.qrCodeView)
     CompoundBarcodeView barcodeView;
+
+    private static final int MY_PERMISSIONS_REQUEST_CAMERA = 1;
     private ReaderScreenContract.Presenter presenter;
 
     @Override
@@ -38,6 +50,48 @@ public class ReaderActivity extends AppCompatActivity implements ReaderScreenCon
     public void init() {
         ButterKnife.bind(this);
         presenter = new ReaderScreenPresenter(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.CAMERA)
+                    != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(ReaderActivity.this,
+                        Manifest.permission.CAMERA)) {
+
+                    ActivityCompat.requestPermissions(ReaderActivity.this,
+                            new String[]{Manifest.permission.CAMERA},
+                            MY_PERMISSIONS_REQUEST_CAMERA);
+                } else {
+                    // No explanation needed, we can request the permission.
+                    ActivityCompat.requestPermissions(ReaderActivity.this,
+                            new String[]{Manifest.permission.CAMERA},
+                            MY_PERMISSIONS_REQUEST_CAMERA);
+                }
+            } else {
+
+                startQRCodeScanning();
+                barcodeView.resume();
+            }
+        } else {
+
+            startQRCodeScanning();
+            barcodeView.resume();
+        }
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        barcodeView.pause();
+    }
+
+    private void startQRCodeScanning() {
+        barcodeView.setVisibility(View.VISIBLE);
         barcodeView.decodeContinuous(new BarcodeCallback() {
             @Override
             public void barcodeResult(BarcodeResult result) {
@@ -61,14 +115,16 @@ public class ReaderActivity extends AppCompatActivity implements ReaderScreenCon
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        barcodeView.resume();
-    }
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_CAMERA:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        barcodeView.pause();
+                    startQRCodeScanning();
+                    barcodeView.resume();
+                } else {
+                    Toast.makeText(getBaseContext(), "Camera Permission Required! Kindly change from App Settings", Toast.LENGTH_SHORT).show();
+                }
+        }
     }
 }
